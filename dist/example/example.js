33182329766c494005aab4373a0d9a4a25c313ac
progrape <chility@qq.com>
2016-04-06 16:27:33 +0800

    #15 修复 demo 在 Android 手机下输入法挡住输入框的 bug

--- dist/example/example.js
+++ dist/example/example.js
@@ -6,7 +6,6 @@ $(function () {
         leaveTimeout: 300
     });
 
-
     // grid
     var home = {
         url: '/',
@@ -255,4 +254,21 @@ $(function () {
         .push(searchbar)
         .setDefault('/')
         .init();
+
+
+    // .container 设置了 overflow 属性, 导致 Android 手机下输入框获取焦点时, 输入法挡住输入框的 bug
+    // 相关 issue: https://github.com/weui/weui/issues/15
+    // 解决方法:
+    // 0. .container 去掉 overflow 属性, 但此 demo 下会引发别的问题
+    // 1. 参考 http://stackoverflow.com/questions/23757345/android-does-not-correctly-scroll-on-input-focus-if-not-body-element
+    //    Android 手机下, input 或 textarea 元素聚焦时, 主动滚一把
+    if (/Android/gi.test(navigator.userAgent)) {
+        window.addEventListener('resize', function () {
+            if (document.activeElement.tagName == 'INPUT' || document.activeElement.tagName == 'TEXTAREA') {
+                window.setTimeout(function () {
+                    document.activeElement.scrollIntoViewIfNeeded();
+                }, 0);
+            }
+        })
+    }
 });
