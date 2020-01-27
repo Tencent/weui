44b7f9aa5801e06b3bbacd36087f8b4486a5748e
bearyan <bearyan@tencent.com>
2020-01-27 15:26:23 +0800

    加入stylelint

---
+++ .stylelintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+    'extends': 'stylelint-config-standard',
+
+    'rules': {
+        'indentation': 4,
+        'selector-pseudo-element-colon-notation': 'single',
+        'no-descending-specificity': null,
+        'no-eol-whitespace': null,
+        'block-no-empty': null,
+        'rule-empty-line-before': null,
+        'max-empty-lines': null,
+        'function-calc-no-invalid': null,
+        'declaration-block-no-duplicate-properties': null,
+        'declaration-block-no-shorthand-property-overrides': null,
+    },
+};
